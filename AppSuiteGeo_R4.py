342f3bdde5e3ff5551d61b76cb8860a2ac882d73
Jose Alfredo Guerrero castro <83942628+Chepe1980@users.noreply.github.com>
2025-08-20 17:41:21 -0600

    Update AppSuiteGeo_R4.py

--- AppSuiteGeo_R4.py
+++ AppSuiteGeo_R4.py
@@ -286,8 +286,8 @@ if app_mode == "Home":
             "name": "XuPayneVernikKachvMod",
             "description": "Rock physics modeling using Xu-Payne, Vernik-Kachanov methods",
             "color": "#d35400",
-            "logo": Image.open("logoApps.png")
-            #"logo": "modules/logos/xupayne_logo.png"
+            "logo": "modules/logos/xupayne_logo.png"
+           
         }
     ]
     
